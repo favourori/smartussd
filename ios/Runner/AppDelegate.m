6bc6cd4a026a0250b19d611d4d0b2c32766040d8
Nelson Bassey <nellybaz10@gmail.com>
2020-03-09 17:29:33 +0200

    update depreciated ios openurl api

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -2,7 +2,7 @@
 #include "GeneratedPluginRegistrant.h"
 #import <Flutter/Flutter.h>
 #import <UIKit/UIKit.h>
-#import "Runner-Swift.h"
+//#import "Runner-Swift.h"
 #import "NokandaImageRecViewController.h"
 
 
@@ -58,7 +58,7 @@ UIImagePickerController *_imagePickerController;
 //            NokandaImageRecViewController *nokand = [NokandaImageRecViewController alloc];
             
 //            [nokand getImage];
-            [weakSelf takeImage];
+//            [weakSelf takeImage];
             
             
             
@@ -86,87 +86,88 @@ UIImagePickerController *_imagePickerController;
     str1 = [str1 stringByAppendingString:@"#"];
 
 
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:str1]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:str1] options:@{} completionHandler:nil];
+    // [[UIApplication sharedApplication] openURL:URL options:@{} completionHandler:nil];
     return 1;
 }
 
-- (void)recogniseImage:(UIImage *)uiImage{
-    printf(" === >>>>>recognise text called \n");
-    FIRVision *vision = [FIRVision vision];
-    FIRVisionTextRecognizer *textRecognizer = [vision onDeviceTextRecognizer];
+//- (void)recogniseImage:(UIImage *)uiImage{
+//    printf(" === >>>>>recognise text called \n");
+//    FIRVision *vision = [FIRVision vision];
+//    FIRVisionTextRecognizer *textRecognizer = [vision onDeviceTextRecognizer];
     
-    FIRVisionImage *image = [[FIRVisionImage alloc] initWithImage:uiImage];
+//    FIRVisionImage *image = [[FIRVisionImage alloc] initWithImage:uiImage];
     
-    [textRecognizer processImage:image
-                      completion:^(FIRVisionText *_Nullable result,
-                                   NSError *_Nullable error) {
-                          if (error != nil || result == nil) {
-                              // ...
-                              printf("===>>>> reuslt is nil \n");
-                              return;
-                          }
-                          
-                          // Recognized text
-                          NSString *resultText = result.text;
-                          printf("%s \n", resultText);
-                          for (FIRVisionTextBlock *block in result.blocks) {
-                              NSString *blockText = block.text;
-//                              printf("%s \n", blockText);
-                              NSNumber *blockConfidence = block.confidence;
-                              printf("%i", blockConfidence);
-                              NSArray<FIRVisionTextRecognizedLanguage *> *blockLanguages = block.recognizedLanguages;
-                              NSArray<NSValue *> *blockCornerPoints = block.cornerPoints;
-                              CGRect blockFrame = block.frame;
-                              for (FIRVisionTextLine *line in block.lines) {
-                                  NSString *lineText = line.text;
-//                                  printf("lines are");
-//                                  printf("%s", lineText);
-                                  NSNumber *lineConfidence = line.confidence;
-                                  NSArray<FIRVisionTextRecognizedLanguage *> *lineLanguages = line.recognizedLanguages;
-                                  NSArray<NSValue *> *lineCornerPoints = line.cornerPoints;
-                                  CGRect lineFrame = line.frame;
-                                  for (FIRVisionTextElement *element in line.elements) {
-                                      NSString *elementText = element.text;
-                                      NSNumber *elementConfidence = element.confidence;
-                                      NSArray<FIRVisionTextRecognizedLanguage *> *elementLanguages = element.recognizedLanguages;
-                                      NSArray<NSValue *> *elementCornerPoints = element.cornerPoints;
-                                      CGRect elementFrame = element.frame;
-                                  }
-                              }
-                          }
-                          
-                          printf(" === >>>> done printing text \n");
-
-                      }];
+//    [textRecognizer processImage:image
+//                      completion:^(FIRVisionText *_Nullable result,
+//                                   NSError *_Nullable error) {
+//                          if (error != nil || result == nil) {
+//                              // ...
+//                              printf("===>>>> reuslt is nil \n");
+//                              return;
+//                          }
+//
+//                          // Recognized text
+//                          NSString *resultText = result.text;
+//                          printf("%s \n", resultText);
+//                          for (FIRVisionTextBlock *block in result.blocks) {
+//                              NSString *blockText = block.text;
+////                              printf("%s \n", blockText);
+//                              NSNumber *blockConfidence = block.confidence;
+//                              printf("%i", blockConfidence);
+//                              NSArray<FIRVisionTextRecognizedLanguage *> *blockLanguages = block.recognizedLanguages;
+//                              NSArray<NSValue *> *blockCornerPoints = block.cornerPoints;
+//                              CGRect blockFrame = block.frame;
+//                              for (FIRVisionTextLine *line in block.lines) {
+//                                  NSString *lineText = line.text;
+////                                  printf("lines are");
+////                                  printf("%s", lineText);
+//                                  NSNumber *lineConfidence = line.confidence;
+//                                  NSArray<FIRVisionTextRecognizedLanguage *> *lineLanguages = line.recognizedLanguages;
+//                                  NSArray<NSValue *> *lineCornerPoints = line.cornerPoints;
+//                                  CGRect lineFrame = line.frame;
+//                                  for (FIRVisionTextElement *element in line.elements) {
+//                                      NSString *elementText = element.text;
+//                                      NSNumber *elementConfidence = element.confidence;
+//                                      NSArray<FIRVisionTextRecognizedLanguage *> *elementLanguages = element.recognizedLanguages;
+//                                      NSArray<NSValue *> *elementCornerPoints = element.cornerPoints;
+//                                      CGRect elementFrame = element.frame;
+//                                  }
+//                              }
+//                          }
+//
+//                          printf(" === >>>> done printing text \n");
+//
+//                      }];
     
 //    return 1;
-}
+//}
 
 
-- (void)takeImage
-{
-    printf(" ===> calling getImage in ObjC class \n");
-    
-    
-    
-    _imagePickerController = [[UIImagePickerController alloc] init];
-    _imagePickerController.delegate = self;
-    _imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
-    _imagePickerController.allowsEditing = NO;
-    
-   FlutterViewController* _viewController = (FlutterViewController*)self.window.rootViewController;
-    [_viewController presentViewController:_imagePickerController animated:YES completion:nil];
-    printf(" === >>> reached here \n");
+//- (void)takeImage
+//{
+//    printf(" ===> calling getImage in ObjC class \n");
+//
+//
+//
+//    _imagePickerController = [[UIImagePickerController alloc] init];
+//    _imagePickerController.delegate = self;
+//    _imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+//    _imagePickerController.allowsEditing = NO;
+//
+//   FlutterViewController* _viewController = (FlutterViewController*)self.window.rootViewController;
+//    [_viewController presentViewController:_imagePickerController animated:YES completion:nil];
+//    printf(" === >>> reached here \n");
+//
+//}
 
-}
-
-- (void)imagePickerController:(UIImagePickerController *) Picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-    printf("==>>> take image controller completed \n");
-    [_imagePickerController dismissViewControllerAnimated:YES completion:nil];
-    UIImage *image=[info objectForKey:UIImagePickerControllerOriginalImage];
-    [self recogniseImage:image];
-
-}
+//- (void)imagePickerController:(UIImagePickerController *) Picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+//    printf("==>>> take image controller completed \n");
+//    [_imagePickerController dismissViewControllerAnimated:YES completion:nil];
+//    UIImage *image=[info objectForKey:UIImagePickerControllerOriginalImage];
+//    [self recogniseImage:image];
+//
+//}
 
 
 
